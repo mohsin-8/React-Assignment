68d22ca789bababfe71f059e6fc267242c8513da
mohsin-8 <mohsinalikhan518@gmail.com>
2022-02-11 20:12:36 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// components
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+}
+
+export default App;
